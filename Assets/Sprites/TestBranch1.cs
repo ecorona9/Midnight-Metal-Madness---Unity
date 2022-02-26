b4c2fa2afc89dbb99de086123c6f18a8fa30edcf
AltBrian379 <46946272+AltBrian379@users.noreply.github.com>
2022-02-26 09:37:34 -0800

    Uploading Test file.
    Uploading the first test file for testbranch one for merging.

---
+++ Assets/Sprites/TestBranch1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This is just a test file. Going to see how merging affects it. Branch1
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
