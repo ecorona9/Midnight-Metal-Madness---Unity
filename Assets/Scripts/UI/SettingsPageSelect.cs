10fb0fbe300463cdbd4e7a1374aec4bf0e155248
ericchu1329 <kiyuen88ec@gmail.com>
2022-04-16 15:14:37 -0700

    Implemented SettingsPageSelect
    displays the current section of the settings menu. Pages are faded in and out when swapping between different pages

--- Assets/Scripts/UI/SettingsPageSelect.cs
+++ Assets/Scripts/UI/SettingsPageSelect.cs
@@ -1,6 +1,7 @@
 /* SUMMARY:
  * SettingsPageSelect manages which page to display in the settings menu
  */
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,17 +9,19 @@ namespace MidnightMetalMadness.UI
 {
     public class SettingsPageSelect : MonoBehaviour
     {        
-        [SerializeField] private CanvasGroup keybinds_page;     // page 0
-        [SerializeField] private CanvasGroup volume_page;       // page 1
-        [SerializeField] private CanvasGroup quality_page;      // page 2
-        [SerializeField] private CanvasGroup help_page;         // page 3
+        [SerializeField] private CanvasGroup keybinds_page;     
+        [SerializeField] private CanvasGroup quality_page;   
+        [SerializeField] private CanvasGroup volume_page;                 
+        [SerializeField] private CanvasGroup help_page;
+        [SerializeField] private float fade_duration = 0.25f;
 
         private CanvasGroup current_page;
 
+        // keybinds page is first option, so will be displayed by default
         public void OnSettingMenuEnabled()
         {
             if (keybinds_page == null) return;
-            // keybinds page is first option, so will be displayed by default
+            
             keybinds_page.alpha = 1f;
             volume_page.alpha = quality_page.alpha = help_page.alpha = 0f;
             current_page = keybinds_page;
@@ -30,19 +33,8 @@ namespace MidnightMetalMadness.UI
 
             if (current_page != keybinds_page)
             {
-                FadeOutSelectedPage();
-                FadeInSelectedPage(keybinds_page);
-            }
-        }
-
-        public void EnableVolumePage()
-        {
-            if (volume_page == null) return;
-
-            if (current_page != volume_page)
-            {
-                FadeOutSelectedPage();
-                FadeInSelectedPage(volume_page);
+                StartCoroutine(FadeOutCurrentPage());
+                StartCoroutine(FadeInSelectedPage(keybinds_page));
             }
         }
 
@@ -52,8 +44,19 @@ namespace MidnightMetalMadness.UI
 
             if (current_page != quality_page)
             {
-                FadeOutSelectedPage();
-                FadeInSelectedPage(quality_page);
+                StartCoroutine(FadeOutCurrentPage());
+                StartCoroutine(FadeInSelectedPage(quality_page));
+            }
+        }
+
+        public void EnableVolumePage()
+        {
+            if (volume_page == null) return;
+
+            if (current_page != volume_page)
+            {
+                StartCoroutine(FadeOutCurrentPage());
+                StartCoroutine(FadeInSelectedPage(volume_page));
             }
         }
 
@@ -63,19 +66,35 @@ namespace MidnightMetalMadness.UI
 
             if (current_page != help_page)
             {
-                FadeOutSelectedPage();
-                FadeInSelectedPage(help_page);
+                StartCoroutine(FadeOutCurrentPage());
+                StartCoroutine(FadeInSelectedPage(help_page));
             }
         }
 
-        private void FadeOutSelectedPage()
+        private IEnumerator FadeOutCurrentPage()
         {
-
+            float init_alpha = 1f;
+            float time = 0f;
+            while (time < fade_duration)
+            {
+                current_page.alpha = Mathf.Lerp(init_alpha, 0f, time / fade_duration);
+                time += Time.unscaledDeltaTime;
+                yield return null;
+            }
+            current_page.alpha = 0f;
         }
 
-        private void FadeInSelectedPage(CanvasGroup page)
+        private IEnumerator FadeInSelectedPage(CanvasGroup page)
         {
-
+            float init_alpha = 0f;
+            float time = 0f;
+            while (time < fade_duration)
+            {
+                page.alpha = Mathf.Lerp(init_alpha, 1f, time / fade_duration);
+                time += Time.unscaledDeltaTime;
+                yield return null;
+            }
+            current_page = page;
         }
     }
 }
